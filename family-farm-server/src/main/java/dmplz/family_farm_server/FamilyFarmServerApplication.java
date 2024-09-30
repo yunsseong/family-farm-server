d1d7192d253dfd89dfe72dca5acf77be4703301b
yunsseong <aster20@hanyang.ac.kr>
2024-10-01 00:58:44 +0900

    chore: spring initializr 프로젝트 가져오기

---
+++ family-farm-server/src/main/java/dmplz/family_farm_server/FamilyFarmServerApplication.java
@@ -0,0 +1,13 @@
+package dmplz.family_farm_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyFarmServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyFarmServerApplication.class, args);
+	}
+
+}
---
+++ family-farm-server/src/test/java/dmplz/family_farm_server/FamilyFarmServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dmplz.family_farm_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyFarmServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
