756322fd3e1ba5f52f267b46028fbb85517456d8
yunsseong <aster20@hanyang.ac.kr>
2024-10-03 00:21:45 +0900

    feat: Member, Family 모델 구현

---
+++ family-farm-server/src/main/java/dmplz/family_farm_server/user/model/Member.java
@@ -0,0 +1,48 @@
+package dmplz.family_farm_server.user.model;
+
+import dmplz.family_farm_server.family.model.Family;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Member {
+
+	@Id
+	@Column(name = "member_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long memberId;
+
+	private String nickname;
+
+	@ManyToOne
+	@JoinColumn(name = "family_id")
+	private Family family;
+
+	public Member(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public void allocateFamily(Family family) {
+		this.family = family;
+		family.addMember(this);
+	}
+
+	@Override
+	public String toString() {
+		return "Member{" +
+			"memberId=" + memberId +
+			", nickname='" + nickname + '\'' +
+			", family=" + family +
+			'}';
+	}
+}
