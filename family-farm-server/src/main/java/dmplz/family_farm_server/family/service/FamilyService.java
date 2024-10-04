c62183ad6d7f5fc84efbd0f0f135e755af0df9f6
yunsseong <aster20@hanyang.ac.kr>
2024-10-04 10:53:56 +0900

    chore: @Transactional 어노테이션 적용

--- family-farm-server/src/main/java/dmplz/family_farm_server/family/service/FamilyService.java
+++ family-farm-server/src/main/java/dmplz/family_farm_server/family/service/FamilyService.java
@@ -5,6 +5,7 @@ import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import dmplz.family_farm_server.family.model.Family;
 import dmplz.family_farm_server.family.repository.FamilyRepository;
@@ -18,32 +19,38 @@ public class FamilyService {
 	private final FamilyRepository familyRepository;
 	private final InviteCodeService inviteCodeService;
 
+	@Transactional(readOnly = true)
 	public Family getFamily(Long id) {
 		return familyRepository.findById(id)
 			.orElseThrow(NoSuchElementException::new);
 	}
 
+	@Transactional(readOnly = true)
 	public List<Family> getAllFamily() {
 		return familyRepository.findAll();
 	}
 
+	@Transactional(readOnly = true)
 	public Family getFamilyByInviteCode(String inviteCode) {
 		return familyRepository.findByInviteCode(inviteCode)
 			.orElseThrow(NoSuchElementException::new);
 	}
 
+	@Transactional
 	public Family createNewFamily() {
 		Family family = new Family(inviteCodeService.generateInviteCode());
 		familyRepository.save(family);
 		return family;
 	}
 
+	@Transactional
 	public Family updateFamily(Family family) {
 		familyRepository.findById(family.getFamilyId())
 			.orElseThrow(NoSuchElementException::new);
 		return familyRepository.save(family);
 	}
 
+	@Transactional
 	public void deleteFamily(Long id) {
 		Family findFamily = familyRepository.findById(id)
 			.orElseThrow(NoSuchElementException::new);
